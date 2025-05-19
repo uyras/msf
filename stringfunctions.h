0f844abacba7b8000f0c68c0dc66b0187f08f60f
Yuriy Shevchenko <150030@mail.ru>
2025-05-19 17:17:52 +0300

    Исправлены устаревшие реализации функций работы со строками

--- stringfunctions.h
+++ stringfunctions.h
@@ -6,16 +6,18 @@
 #include <functional> 
 #include <string>
 
-/// trim from start (in place)
-static inline void ltrim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))));
+// trim from start (in place)
+inline void ltrim(std::string &s) {
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }));
 }
 
 // trim from end (in place)
-static inline void rtrim(std::string &s) {
-    s.erase(std::find_if(s.rbegin(), s.rend(),
-            std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+inline void rtrim(std::string &s) {
+    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    }).base(), s.end());
 }
 
 // trim from both ends (in place)
